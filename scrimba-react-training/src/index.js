c53b8b05478eb7c3047fb3c4a003601978966b6d
Keian Barton <kb_spy1@hotmail.com>
2019-09-04 12:36:11 +0100

    Functional component

--- scrimba-react-training/src/index.js
+++ scrimba-react-training/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react'; // used to interpret JSX
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+function MyApp() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
 
 // JSX
 ReactDOM.render(
-    <ul>
-        <li>Completing exercise</li>
-        <li>Foo</li>
-        <li>Bar</li>
-    </ul>,
+    <MyApp />,
     document.getElementById("root")
 );
-// You cannot render adjacent JSX elements unless they are wrapped in a single element e.g. ReactDom.Render(<h1>foo</h1><p>bar</p>, ....)
