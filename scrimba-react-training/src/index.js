2210c2fc2b9bca00f27af0e865bc5b0eb9f96ec1
Keian Barton <kb_spy1@hotmail.com>
2019-09-04 12:30:18 +0100

    JSX Introduction

--- scrimba-react-training/src/index.js
+++ scrimba-react-training/src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from 'react'; // used to interpret JSX
 import ReactDOM from 'react-dom';
 
+// JSX
 ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+// You cannot render adjacent JSX elements unless they are wrapped in a single element e.g. ReactDom.Render(<h1>foo</h1><p>bar</p>, ....)
