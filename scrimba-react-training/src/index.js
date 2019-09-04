eece4d0fc9372ddf273bba7c8a9fdb1a62c8cbfc
Keian Barton <kb_spy1@hotmail.com>
2019-09-04 12:33:16 +0100

    Rendering list

--- scrimba-react-training/src/index.js
+++ scrimba-react-training/src/index.js
@@ -2,5 +2,12 @@ import React from 'react'; // used to interpret JSX
 import ReactDOM from 'react-dom';
 
 // JSX
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+ReactDOM.render(
+    <ul>
+        <li>Completing exercise</li>
+        <li>Foo</li>
+        <li>Bar</li>
+    </ul>,
+    document.getElementById("root")
+);
 // You cannot render adjacent JSX elements unless they are wrapped in a single element e.g. ReactDom.Render(<h1>foo</h1><p>bar</p>, ....)
