9504bc25b67ce7e871531ce82467ddc0a16ca516
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 10:16:19 +0100

    React props

--- scrimba-react-training/src/components/MainContent.js
+++ scrimba-react-training/src/components/MainContent.js
@@ -1,14 +1,37 @@
 import React from "react";
 
-import ToDoItem from "./ToDoItem";
+import ContactCard from "./ContactCard";
 
 function MainContent() {
     return (
         <div className="todo-list">
-            <ToDoItem />
-            <ToDoItem />
-            <ToDoItem />
-            <ToDoItem />
+            <ContactCard 
+                name="Mr. Whiskerson" 
+                imgUrl="http://placekitten.com/300/200" 
+                phone="(212) 555-1234" 
+                email="mr.whiskaz@catnap.meow"
+            />
+            
+            <ContactCard 
+                name="Fluffykins" 
+                imgUrl="http://placekitten.com/400/200" 
+                phone="(212) 555-2345" 
+                email="fluff@me.com"
+            />
+            
+            <ContactCard 
+                name="Destroyer" 
+                imgUrl="http://placekitten.com/400/300" 
+                phone="(212) 555-3456" 
+                email="ofworlds@yahoo.com"
+            />
+            
+            <ContactCard 
+                name="Felix" 
+                imgUrl="http://placekitten.com/200/100" 
+                phone="(212) 555-4567" 
+                email="thecat@hotmail.com"
+            />
         </div>
     );
 };
