270744062edefb32fec2d5ef65abf0f63ae64152
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 16:30:50 +0100

    Working to-do app using App handling events for child components

--- scrimba-react-training/src/components/App.js
+++ scrimba-react-training/src/components/App.js
@@ -8,17 +8,27 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = { todos: todosData };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleClick = () => alert("Button Clicked!");
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            });
+            return updatedTodos;
+        });
+    }
 
     render = () => {
-        let todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />);
+        let todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>);
         return <>
             <div className="todo-list">
                 {todoItems}
             </div>
-            <button onClick={this.handleClick}>Click Me</button>
         </>;
     }
 
--- scrimba-react-training/src/components/TodoItem.js
+++ scrimba-react-training/src/components/TodoItem.js
@@ -1,23 +1,11 @@
 import React, { Component } from "react"
 
 class TodoItem extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            completed: props.item.completed
-        }
-        this.handleSelectionChange = this.handleSelectionChange.bind(this); // binding "this" within handleSelectionChange to be the "this" referring to this instance
-    }
-
-    handleSelectionChange() {
-        this.setState(prevState => { return { completed: !prevState.completed }; }); // use argument to access previous state
-    }
-
     render() {
         let item = this.props.item;
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={this.state.completed} onChange={this.handleSelectionChange}/>
+                <input type="checkbox" checked={this.props.completed} onChange={() => { this.props.handleChange(item.id)}}/>
                 <p>{item.text}</p>
             </div>
         );
