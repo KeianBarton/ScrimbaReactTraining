ffb0e798065052d5883ca994c9d4a9134d049a36
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 10:04:53 +0100

    Parent/child components

--- scrimba-react-training/src/index.js
+++ scrimba-react-training/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from './components/App';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
     <App />,
