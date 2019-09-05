2981b028edf819e0d09fe7870002a8084f52990b
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 10:41:43 +0100

    Basic conditional formatting using style

--- scrimba-react-training/src/components/ContactCard.js
+++ scrimba-react-training/src/components/ContactCard.js
@@ -5,9 +5,9 @@ function ContactCard(props) {
     return (
         <div className="contact-card">
             <img src={contact.imgUrl} alt={`Image of ${contact.name}`}/>
-            <h3 style={{display: contact.name ? "block" : "none"}}>{contact.name}</h3>
-            <p style={{display: contact.phone ? "block" : "none"}}>{contact.phone}</p>
-            <p style={{display: contact.email ? "block" : "none"}}>{contact.email}</p>
+            <h3 style={{display: !props.question && "none"}}>{contact.name}</h3>
+            <p style={{display: !props.question && "none"}}>{contact.phone}</p>
+            <p style={{display: !props.question && "none"}}>{contact.email}</p>
         </div>
     );
 };
