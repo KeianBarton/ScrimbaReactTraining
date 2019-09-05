2a97815e15c781e0bda7fa32e736c3963a4eaae0
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 11:10:03 +0100

    Adding a list of data via mapping components into JSX

--- scrimba-react-training/src/components/MainContent.js
+++ scrimba-react-training/src/components/MainContent.js
@@ -1,40 +1,13 @@
 import React from "react";
 
 import ContactCard from "./ContactCard";
+import contactsData from "../contactsData";
 
 function MainContent() {
+    const contactsComponents = contactsData.map(contact => <ContactCard contact={contact} />);
     return (
-        <div className="todo-list">
-            <ContactCard 
-                contact={{
-                    name: "Mr. Whiskerson",
-                    imgUrl: "http://placekitten.com/300/200",
-                    phone: "(212) 555-1234",
-                    email: "mr.whiskaz@catnap.meow"
-                }}
-            />
-            
-            <ContactCard 
-                contact={{
-                    name: "Fluffykins",
-                    imgUrl: "http://placekitten.com/400/200",
-                    phone: "(212) 555-2345",
-                    email: "fluff@me.com"
-                }}
-            />
-            
-            <ContactCard
-                contact={{
-                    name: "Destroyer",
-                    imgUrl: "http://placekitten.com/400/300",
-                    phone: "(212) 555-3456",
-                    email: "ofworlds@yahoo.com"
-                }}
-            />
-            
-            <ContactCard 
-                contact={{imgUrl: "http://placekitten.com/200/100"}}
-            />
+        <div className="contacts-list">
+            {contactsComponents}
         </div>
     );
 };
