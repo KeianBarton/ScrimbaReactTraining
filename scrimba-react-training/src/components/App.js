217af6f3977cc49428ab63e2d5bcea78a2023ce9
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 15:13:19 +0100

    Todo app using state

--- scrimba-react-training/src/components/App.js
+++ scrimba-react-training/src/components/App.js
@@ -1,35 +1,17 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+
+import TodoItem from "./TodoItem";
+import todosData from "../todosData";
 
 class App extends Component {
+
     constructor(props) {
         super(props);
-        // state are mutable variables initialised and managed by the component and can be updated everywhere
-        // props are immutable variables passed from parent components
-        this.state = {
-            answer: "Yes"
-        };
+        this.state = todosData.map(item => <TodoItem key={item.id} item={item} />);
     }
 
-    render() {
-        return (
-            <>
-                <TestComponent answer={this.state.answer} />
-            </>
-        );
-    }
+    render = () => <div className="todo-list">{this.state}</div>;
+
 }
-
-class TestComponent extends Component {
-    render() {
-        return (
-            <>
-                <h1>{this.props.answer}</h1>
-            </>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById("root")); // note we can't use props on the outermost element
 
 export default App;
