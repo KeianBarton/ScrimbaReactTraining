2ce5242f583aef84d6cdf53bbcb1b2f0c3169319
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 16:14:09 +0100

    setState in action with event handling

--- scrimba-react-training/src/components/TodoItem.js
+++ scrimba-react-training/src/components/TodoItem.js
@@ -6,13 +6,18 @@ class TodoItem extends Component {
         this.state = {
             completed: props.item.completed
         }
+        this.handleSelectionChange = this.handleSelectionChange.bind(this); // binding "this" within handleSelectionChange to be the "this" referring to this instance
+    }
+
+    handleSelectionChange() {
+        this.setState(prevState => { return { completed: !prevState.completed }; }); // use argument to access previous state
     }
 
     render() {
         let item = this.props.item;
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={item.completed} onChange={console.log("Changed!")}/>
+                <input type="checkbox" checked={this.state.completed} onChange={this.handleSelectionChange}/>
                 <p>{item.text}</p>
             </div>
         );
