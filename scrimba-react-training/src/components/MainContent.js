4d4ff50ab27f0483aa79a6627beb1593356ea469
Keian Barton <kb_spy1@hotmail.com>
2019-09-05 11:40:27 +0100

    ToDo list

--- scrimba-react-training/src/components/MainContent.js
+++ scrimba-react-training/src/components/MainContent.js
@@ -1,20 +1,11 @@
 import React from "react";
 
-import ContactCard from "./ContactCard";
-import contactsData from "../contactsData";
+import TodoItem from "./TodoItem";
+import todosData from "../todosData";
 
 function MainContent() {
-    const contactsComponents = contactsData.map(contact => <ContactCard key={contact.id} contact={contact} />);
-    return <div className="contacts-list">{contactsComponents}</div>;
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+    return <div className="todo-list">{todoItems}</div>;
 };
 
 export default MainContent;
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex
