f27c026bd8eb6b57781eb1c3839faa4327346256
Keian Barton <kb_spy1@hotmail.com>
2019-09-06 15:27:39 +0100

    Finished to-do app

--- scrimba-react-training/src/components/ConditionalExample.js
+++ scrimba-react-training/src/components/ConditionalExample.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 
 class ConditionalExample extends Component {
     render() {
-        return //(this.props.isLoading) ? <h1>Loading....</h1> : <h1>Loaded</h1> 
-        <h1>Loaded</h1>
+        return <>{/*(this.props.isLoading) ? <h1>Loading....</h1> : <h1>Loaded</h1>*/} 
+            <h1>Loaded</h1>
+        </>
     }
 }
 
--- scrimba-react-training/src/components/TodoItem.js
+++ scrimba-react-training/src/components/TodoItem.js
@@ -3,10 +3,15 @@ import React, { Component } from "react"
 class TodoItem extends Component {
     render() {
         let item = this.props.item;
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#cdcdcd",
+            textDecoration: "line-through"
+        }
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={this.props.completed} onChange={() => { this.props.handleChange(item.id)}}/>
-                <p>{item.text}</p>
+                <input type="checkbox" checked={item.completed} onChange={() => { this.props.handleChange(item.id)}}/>
+                <p style={item.completed ? completedStyle : null}>{item.text}</p>
             </div>
         );
     }
