1defeb1aacf0f11c3d952672869d42540a40a2eb
Keian Barton <kb_spy1@hotmail.com>
2019-09-06 12:11:36 +0100

    Introducion to lifecycle hooks

--- scrimba-react-training/src/components/App.js
+++ scrimba-react-training/src/components/App.js
@@ -5,6 +5,10 @@ import todosData from "../todosData";
 
 class App extends Component {
 
+    // https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+    // https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+    // React Lifecycle methods
+
     constructor(props) {
         super(props);
         this.state = { todos: todosData };
@@ -23,6 +27,30 @@ class App extends Component {
         });
     }
 
+    // Just mounted to screen - very first time component shown on scren
+    componentDidMount() {
+        // GET the data I need to correctly display
+    }
+
+
+    // TO BE DEPRECATED - Props received from parent component - runs on mount and every time props changed
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        if (nextProps.whatever !== this.props.whatever) {
+            // do something important here
+        }
+    }
+
+    // Invoked before rendering when new props or state are received - defaults to true - not called when forceUpdate is used
+    shouldComponentUpdate(nextProps, nextState) {
+    }
+
+    // Invoked when component unmounts from screen
+    componentWillUnmount() {
+        // teardown or cleanup your code before your component disappears
+        // (E.g. remove event listener)
+    }
+
+    // Any time React determines state or props changes, ernder is run
     render = () => {
         let todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>);
         return <>
@@ -31,6 +59,7 @@ class App extends Component {
             </div>
         </>;
     }
+
 
 }
 
