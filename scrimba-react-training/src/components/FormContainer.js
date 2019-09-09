de5a634f9265b38e9377fbcb74b5ef233daa7cd4
Keian Barton <kb_spy1@hotmail.com>
2019-09-09 15:40:26 +0100

    Splitting components into smart/dumb components is sometimes recommended

--- scrimba-react-training/src/components/FormContainer.js
+++ scrimba-react-training/src/components/FormContainer.js
@@ -35,6 +35,7 @@ class FormContainer extends Component {
 
     render() {
         return (
+            // https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
             <FormComponent
                 handleFormChange={this.handleFormChange}
                 data={this.state}
